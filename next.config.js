46a44907f791811b24c2550915385afaedce664e
Sunny Cletus <scletus@gmail.com>
2020-08-10 09:21:33 +0100

    changes made

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    FIREBASE_API_KEY: "AIzaSyC5cNZ9pwoZftFNLtmSA-wiyIJDa4oOBBQ",
+    FIREBASE_AUTH_DOMAIN: "betterlifesavings-dev.firebaseapp.com",
+    FIREBASE_DATABASE_URL: "https://betterlifesavings-dev.firebaseio.com",
+    FIREBASE_PROJECT_ID: "betterlifesavings-dev",
+    FIREBASE_STORAGE_BUCKET: "betterlifesavings-dev.appspot.com",
+    FIREBASE_MESSAGING_SENDER_ID: "327003514820",
+    FIREBASE_APP_ID: "1:327003514820:web:216e1df477f9b4de14e691",
+    FIREBASE_MEASUREMENT_ID: "G-930DCXE3KC",
+    FIREBASE_PUBLIC_VAPID_KEY:
+      "BDCDzOQS1GNCBNI6IfK37Haq88yU5949jI56Y4XVP5AQsNGqsNVcd4i-maopDIU4CGHpBdyEkHeZAZs8Kl1V5II",
+  }
+};
